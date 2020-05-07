05c5edfec3ea88ec0851a0c1c7c6cdc56b009ee4
Poyraz Alkan <poyrazdaka@gmail.com>
2020-05-07 05:47:45 +0300

    default fetch fix
    default 10 row fetch

--- datatable2sql.js
+++ datatable2sql.js
@@ -1,18 +1,4 @@
-//TR
-/*
-Bu modül datatable için express.js üzerinde ajax yaparken search parametrelerini yönetmek ve sql cümleciği hazırlamak için geliştirilmiştir.
-https://datatables.net/ acık kaynak kodlu bir veri listeleme ve tablo yönetim aracıdır.
-https://expressjs.com/  Node js için geliştirilmiş hızlı, yönlendirme yapmayan, minimalist bir web geliştirme ortamıdır.
-Geliştiren:Poyraz Alkan 
-*/
-//ENG
-/*
 
-This module is developed for managing search parameters and preparing sql clause while doing ajax on express.js for datatable.
-https://datatables.net/ is a open source data list and table manager tools.
-https://expressjs.com/ Fast, unopinionated, minimalist web framework for Node.js
-Develop BY :Poyraz Alkan
-*/
 
 exCOL = function (colname) {
     return function (req, res, next) {
@@ -46,7 +32,7 @@ dataTableQuery2SQL = function(custom_where){
 
         if(custom_where){if (result == " WHERE "){ result+=custom_where;}else{  result +=" and "+ custom_where }}
         if (result == " WHERE ") { result = ""; }
-    
+        if(req.body["length"] ==-1){req.body["length"]=10}
         result += " Order by " + req.body["columns[" + req.body["order[0][column]"] + "][data]"] + " " + req.body["order[0][dir]"] + " OFFSET " + req.body["start"] + " ROWS FETCH NEXT " + req.body["length"] + " ROWS ONLY OPTION (RECOMPILE);"
         req.dataTableQuery2SQL = result;
         next();
